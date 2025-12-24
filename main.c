be3868586914230c96917a79565a5f83658776ba
Cristopher Reyes <reyescristopher43@gmail.com>
2025-12-24 14:16:14 -0600

    feat: add application logic using button driver

--- main.c
+++ main.c
@@ -3,6 +3,13 @@
 #include "gpio.h"
 #define __XTAL_FREQ 20000000
 
+gpio_t led = {
+	.tris = &TRISD,
+	.lat = &LATD,
+	.port = &PORTD,
+	.pin = 0
+};
+
 void __interrupt() isr(void)
 {
 	if(INTCONbits.INT0IF)
@@ -14,6 +21,19 @@ void __interrupt() isr(void)
 
 int main(){
 	button_init();
+	gpio_init(&led,GPIO_OUTPUT);
+
+
+	while(1)
+	{
+		button_process();
+
+		if(button_get_state() == BUTTON_PRESSED)
+		{
+			gpio_toggle(&led);
+		}
+		
+	}
 
 	return 1;
 }
